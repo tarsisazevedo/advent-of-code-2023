22510ae21f7a13107b4c3e3e907a51d4f0bdaa70
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2023-12-01 21:58:51 -0300

    code a little bit cleaner

--- 1/task1.py
+++ 1/task1.py
@@ -1,3 +1,16 @@
+numbers_as_texts = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0
+}
+
 def replace_numbers_as_texts(line, numbers_as_texts):
     for char in line:
         for k in numbers_as_texts.keys():
@@ -8,28 +21,14 @@ def replace_numbers_as_texts(line, numbers_as_texts):
 
 def find_numbers(input):
     numbers = []
-    numbers_as_texts = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-        "zero": 0
-    }
     for line in input.splitlines():
         line_numbers  = ""
         line = replace_numbers_as_texts(line, numbers_as_texts)
-        print(line)
         for char in line:
             if char.isdigit():
                 line_numbers += char
         if line_numbers:
             number = int(line_numbers[0]+line_numbers[-1]) 
-            print(str(number)+"\n"+100*"-")
             numbers.append(number)
     return sum(numbers)
 
--- 1/test_task1.py
+++ 1/test_task1.py
@@ -1,4 +1,4 @@
-from task1 import find_numbers, replace_numbers_as_texts
+from task1 import find_numbers, numbers_as_texts, replace_numbers_as_texts
 
 
 def test_find_numbers():
@@ -23,32 +23,10 @@ zoneight234
 
 def test_replace_numbers_as_texts():
     line = "two1nine"
-    numbers_as_texts = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
-    }
     line = replace_numbers_as_texts(line, numbers_as_texts)
     assert line == "219"
 
 def test_replace_numbers_as_texts_with_lower_wrong_number():
     line = "eightwothree"
-    numbers_as_texts = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
-    }
     line = replace_numbers_as_texts(line, numbers_as_texts)
     assert line == "8wo3"
