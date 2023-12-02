bfafc8f5f81212adadc66a805970a23802a8862b
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2023-12-02 11:17:44 -0300

    task2 part2 done

--- 2/task2.py
+++ 2/task2.py
@@ -28,8 +28,38 @@ def get_valid_lines(input):
             valid_lines.append(int(game_id))
     return valid_lines
 
+def get_max_number_of_each_cube(game):
+    """Return a dictionary with the max number of each cube."""
+    max_number_of_each_cube = {}
+    subsets = game.split(";")
+    for subset in subsets:
+        cubes = subset.split(",")
+        for cube in cubes:
+            quantity, color = cube.strip().split(" ")
+            max_number_of_each_cube[color] = max(max_number_of_each_cube.get(color, 0), int(quantity))
+    return max_number_of_each_cube
+
+def get_power_from_cubes(cubes):
+    """Return the power from the cubes."""
+    power = 1
+    for _, quantity in cubes.items():
+        power *= quantity
+    return power
+
+def get_sum_of_all_game_powers(input):
+    powers = []
+    lines = input.splitlines()
+    for i, line in enumerate(lines):
+        _, game = line.split(":")
+        max_number_of_each_cube = get_max_number_of_each_cube(game)
+        power = get_power_from_cubes(max_number_of_each_cube)
+        powers.append(power)
+    return sum(powers)
+
+
 if __name__ == "__main__":
     with open("input.txt") as f:
         input = f.read()
         valid_lines = get_valid_lines(input)
         print(sum(valid_lines))
+        print(get_sum_of_all_game_powers(input))
--- 2/test_task2.py
+++ 2/test_task2.py
@@ -1,4 +1,5 @@
-from task2 import get_valid_lines, is_valid_game
+from task2 import (get_max_number_of_each_cube, get_power_from_cubes,
+                   get_sum_of_all_game_powers, get_valid_lines, is_valid_game)
 
 
 def test_valid_line():
@@ -14,3 +15,21 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 def test_valid_game():
     game = "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
     assert is_valid_game(game) == True
+
+def test_get_max_number_of_each_cube():
+    input = """3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"""
+    expected = {"red": 4, "green": 2, "blue": 6}
+    assert get_max_number_of_each_cube(input) == expected
+
+def test_get_power_from_cubes():
+    input = {"red": 4, "green": 2, "blue": 6}
+    assert get_power_from_cubes(input) == 48
+
+def test_get_sum_of_all_game_powers():
+    input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+    expected = 2286
+    assert get_sum_of_all_game_powers(input) == expected
